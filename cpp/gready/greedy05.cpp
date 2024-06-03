36cfe6b10e4d441af3cafbfdaff22ec5f248065d
marc <nguyenvannhv26@gmail.com>
2024-06-03 13:06:42 +0700

    update gready05

--- cpp/gready/greedy05.cpp
+++ cpp/gready/greedy05.cpp
@@ -1,10 +1,36 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+typedef long long ll;
 
 int main() {
 #ifndef ONLINE_JUDGE
   freopen("../input.txt", "r", stdin);
   freopen("../output.txt", "w", stdout);
 #endif
+	
+	int n; cin >> n;
+	int a[n],b[n];
+	priority_queue<ll, vector<ll>, greater<ll> > q;
+	for(int i = 0; i<n; i++){
+		ll x; cin >> x;
+		a[i] = x;
+		q.push(x);
+	}
+	for(int i = 0; i < n; i++){
+		b[i] = q.top();
+		q.pop();
+	}
+	
+	bool ok = true;
+	for(int i = 0; i <= n/2; i++){
+		bool match = a[i] == b[i] || a[i] == b[n - 1 - i];
+		if(!match){
+			ok = false;
+			break;
+		}
+	}
+	if(ok) cout << "YES"; else cout << "NO";
+	
+	
 }
