2c4d8290ead78eefc6a29a97d1244f2643cf35a6
marc-from-lap <nguyenvannhv26@gmail.com>
2024-06-28 00:39:15 +0700

    add more graph problems

--- cpp/self-taught/graph/graph18.cpp
+++ cpp/self-taught/graph/graph18.cpp
@@ -1,5 +1,3 @@
-
-
 #include <bits/stdc++.h>
 #define endl "\n"
 #define ll long long
@@ -10,48 +8,58 @@ const int MOD = 1e9 + 7;
 int n, m;
 vector<int> ke[200005];
 int visited[200005];
+int tplt[200005];
+int dem = 0;
 
 void nhap() {
-  cin >> n >> m;
-  for (int i = 0; i < m; i++) {
-    int x, y;
-    cin >> x >> y;
-    ke[x].push_back(y);
-    ke[y].push_back(x);
-  }
-
-  for (int i = 1; i <= n; i++) {
-    sort(ke[i].begin(), ke[i].end());
-  }
+	cin >> n >> m;
+	for(int i = 1; i <= m; i++) {
+		int x, y;
+		cin >> x >> y;
+		ke[x].push_back(y);
+		ke[y].push_back(x);
+	}
+	for(int i = 1; i <= n; i++) {
+		sort(ke[i].begin(), ke[i].end());
+	}
 }
 
-void DFS(int x, int y) {
-  visited[x] = 1;
-  for (int i : ke[x]) {
-    if (!visited[i]) {
-      visited[i] = 1;
-      DFS(i, y);
-    }
-  }
+void DFS(int u) {
+	visited[u] = dem;
+	for(auto it : ke[u]) {
+		if(!visited[it]) {
+			DFS(it);
+		}
+	}
 }
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 #ifndef ONLINE_JUDGE
-  freopen("../../input.txt", "r", stdin);
-  freopen("../../output.txt", "w", stdout);
+	freopen("../../input.txt", "r", stdin);
+	freopen("../../output.txt", "w", stdout);
 #endif
 
-  nhap();
+	nhap();
+	int Q;
+	cin >> Q;
+	for(int i = 1; i <= n; i++) {
+		if(!visited[i]) {
+			dem++;
+			DFS(i);
+		}
+	}
 
-  int Q;
-  cin >> Q;
-  for (int i = 0; i < Q; i++) {
-    int x, y;
-    cin >> x >> y;
-    memset(visited, 0, sizeof(vistied));
-    DFS(u);
-  }
+
+	while(Q--) {
+		int s, t;
+		cin >> s >> t;
+		if(visited[s] == visited[t]) {
+			cout << 1 << endl;
+		} else {
+			cout << -1 << endl;
+		}
+	}
 }
--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,28 +1,11 @@
-#include <bits/stdc++.h>
 
-using namespace std;
 
-int main() {
-#ifndef ONLINE_JUDGE
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
-#endif
-  cout << "tset";
-  // int n, target, a[100];
-  // cin >> n;
-  // unordered_map<int, int> mp;
-  // for (int i = 0; i < n; i++) {
-  //   cin >> a[i];
-  //   mp[a[i]] = i;
-  // }
+int a[10]; ---
 
-  // cin >> target;
 
-  // for (int i = 0; i < n; i++) {
-  //   int x = target - a[i];
-  //   if (mp[x] > 0) {
-  //     cout << i << " " << mp[x];
-  //     break;
-  //   }
-  // }
-}
+vector<int> b;
+b.push();
+
+pair<int, int> {1: a}
+
+
