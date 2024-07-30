fae575c8d4f3b7f0811a9623e2b5f44d30e07056
marc-from-lap <nguyenvannhv26@gmail.com>
2024-07-31 00:42:34 +0700

    add preparation probelm for dsa final exam 1

--- PTIT - DSA/bai2.cpp
+++ PTIT - DSA/bai2.cpp
@@ -2,35 +2,68 @@
 using namespace std;
 
 struct CanBo {
-  long MaSo;
-  char HoTen[8];
+	long MaSo;
+	char HoTen[8];
 };
 
 struct Node {
-  CanBo Info;
-  Node *Next;
+	CanBo Info;
+	Node *Next;
 };
 
 struct DanhSach {
-  Node *PFirst, *PLast;
+	Node *PFirst, *PLast;
 };
 
+void KhoiTaoDanhSach(DanhSach &ds) {
+	ds->PFirst = NULL;
+	ds->PLast = NULL;
+}
+
 Node *makeNode(CanBo cb) {
-  Node *head = new Node();
-  head->Info = cb;
-  head->Next = NULL;
-  return head;
+	Node *head = new Node();
+	head->Info = cb;
+	head->Next = NULL;
+	return head;
+}
+
+bool KiemTraDanhSachRong(DanhSach ds) {
+	return ds->PFirst == NULL;
+}
+
+void ThemNodeDau(Node **head, CanBo cb, DanhSach &ds) {
+	Node *newNode = makeNode(cb);
+
+	if(ds->PFirst == NULL) {
+		ds.PFirst = newNode;
+		ds.PLast = newNode;
+
+		//Cap nhat node head
+
+	} else {
+		newNode->next = ds->PFirst;
+	}
+	*head = newNode;
 }
 
 int main() {
-  // Node *head = makeNode();
-  while (true) {
-    int selection;
-    cin >> selection;
-    switch (selection) {
-      case 1:
-        cout << 1;
-    }
-  }
-  return 0;
+	DanhSach ds;
+	Node *head = NULL;
+	while (true) {
+		int selection;
+		cin >> selection;
+		switch (selection) {
+			case 1:
+				KhoiTaoDanhSach(ds);
+			case 2:
+				if(KiemTraDanhSachRong(ds)) cout << "Danh sach rong\n";
+				else cout << "Danh sach khong rong\n";
+			case 3:
+				CanBo cb;
+				cin >> cb.MaSo >> cb.HoTen;
+
+
+		}
+	}
+	return 0;
 }
