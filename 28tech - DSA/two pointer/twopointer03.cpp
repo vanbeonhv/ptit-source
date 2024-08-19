101699233c594b854df647d3717bfc0b55111530
marc <nguyenvannhv26@gmail.com>
2024-08-20 00:13:59 +0700

    finish twopointer3

--- 28tech - DSA/two pointer/twopointer03.cpp
+++ 28tech - DSA/two pointer/twopointer03.cpp
@@ -6,49 +6,41 @@ using namespace std;
 const int MOD = 1e9 + 7;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-//  freopen("output.txt", "w", stdout);
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
 #endif
-	int n, m;
-	cin >> n >> m;
-	int a[n], b[m];
+  int n, m;
+  cin >> n >> m;
+  int a[n], b[m];
 
-	for (int &x : a) cin >> x;
-	for (int &x : b) cin >> x;
+  for (int &x : a) cin >> x;
+  for (int &x : b) cin >> x;
 
-	int i = 0, j = 0, total = 0, cnt = 0;
+  int i = 0, j = 0;
+  ll cnt = 0;
 
-	while (i < n && j < m) {
-		if (a[i] == b[j]) {
-			cnt++;
-			i++;
-		} else if (a[i] > b[j]) {
-			total += cnt;
-			if (b[j] != b[j + 1]) {
-				cnt = 0;
-			}
-			j++;
-
-		} else if (a[i] < b[j]) {
-			i++;
-		}
-	}
-
-	while(j < m) {
-		if(a[i - 1] == b[j]) {
-			cnt++;
-		} else {
-			total += cnt;
-			
-			if (b[j] != b[j + 1]) {
-				cnt = 0;
-			}
-		}
-		j++;
-	}
-	cout << total;
+  while (i < n && j < m) {
+    if (a[i] == b[j]) {
+      int d1 = 0, d2 = 0;
+      int x = a[i];
+      while (i < n && a[i] == x) {
+        i++;
+        d1++;
+      }
+      while (j < m && b[j] == x) {
+        j++;
+        d2++;
+      }
+      cnt += 1ll * d1 * d2;
+    } else if (a[i] < b[j]) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+  cout << cnt;
 }
