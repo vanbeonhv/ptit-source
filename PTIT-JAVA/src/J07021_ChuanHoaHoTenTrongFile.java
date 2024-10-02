a0778e0890175e88c19e267b1013a941b9dba595
marc <nguyenvannhv26@gmail.com>
2024-10-02 08:09:13 +0700

    add java problem 7

--- PTIT-JAVA/src/J07021_ChuanHoaHoTenTrongFile.java
+++ PTIT-JAVA/src/J07021_ChuanHoaHoTenTrongFile.java
@@ -1,12 +1,31 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class J07021_ChuanHoaHoTenTrongFile {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new File("DATA.in"));
-        while(in.hasNextLine()){
-
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            if (Objects.equals(line, "END")) {
+                return;
+            }
+            System.out.println(ChuanHoa(line.trim()));
         }
+    }
+
+    public static StringBuilder ChuanHoa(String line) {
+
+        StringTokenizer st = new StringTokenizer(line);
+        StringBuilder sb = new StringBuilder();
+        while (st.hasMoreTokens()) {
+            String s = st.nextToken();
+            sb.append(s.substring(0, 1).toUpperCase())
+                    .append(s.substring(1).toLowerCase())
+                    .append(" ");
+        }
+        return sb;
     }
 }
