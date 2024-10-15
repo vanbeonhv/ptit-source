f5965e5f07985f28c74dffd8cf4e393aab23ab75
marc <nguyenvannhv26@gmail.com>
2024-10-15 22:10:35 +0700

    tmp

---
+++ PTIT-JAVA/src/J07033_Danh_Sach_Sinh_Vien_Trong_File.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class J07033_Danh_Sach_Sinh_Vien_Trong_File {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("SINHVIEN.in"));
+        int k = in.nextInt();
+        while (k-- > 0) {
+
+        }
+    }
+}
